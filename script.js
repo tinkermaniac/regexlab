ec62c399dec518150d93080fbfc6dbf334260033
Ritwik Dadarwal <ritwik@Deus.local>
2025-12-28 04:39:40 +0530

    sdhbdwe

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const textInput = document.getElementById("textInput");
-const regexInput = document.getElementById("regexInput")
+const regexInput = document.getElementById("regexInput");
 const result = document.getElementById("result");
 
+function testRegex() {
+    const text = textInput.value;
+    const pattern = regexInput.value;
+
+    if (!pattern) {
+        result.textContent = "Enter a regex pattern.";
+        return;
+    }
+
+    try {
+        const regex = new RegExp(pattern);
+        const match = regex.test(text);
+
+        result.textContent = match
+        ? "✅ Match found"
+        : "❌ No match";
+    } catch (err) {
+        result.textContent = "⚠️ Invalid regex pattern";
+   }
+}
+
+textInput.addEventListener("input", testRegex);
+regexInput.addEventListener("input", testRegex);
